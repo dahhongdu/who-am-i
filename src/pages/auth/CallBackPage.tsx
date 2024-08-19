d2479f57a466176589495cb0747d7681ef509f20
dahhongdu <dahhongdu@gmail.com>
2024-08-19 20:09:24 +0900

    fix. login 완료 후 redirect page를 빠져나간다.

--- src/pages/auth/CallBackPage.tsx
+++ src/pages/auth/CallBackPage.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import { userApi } from '../../api/services/auth';
 
 const CallBackPage = () => {
+  const navigate = useNavigate();
+
   const parseCode = async () => {
     const code: string = window.location.search.substring(6);
     const isLoggedIn: boolean = await userApi.kakaoAuth(code);
 
     if (isLoggedIn) {
-      alert('로그인 완료!');
+      console.log('로그인이 성공적으로 완료되었습니다.');
+      navigate(-1);
+    } else {
+      console.log('모종의 이유로 로그인이 실패했습니다.. 붐따..');
     }
   };
 
