200db421ac244ec5d1493ee3a2812beba98571a2
dahhongdu <dahhongdu@gmail.com>
2024-08-20 23:29:37 +0900

    chore. isLoading을 없애고 isAccessTokenValid 함수 추상화해서 분리

--- src/contexts/contexts.tsx
+++ src/contexts/contexts.tsx
@@ -6,32 +6,28 @@ export const LoginStateContext = createContext<LoginStateContextType | null>(
   null
 );
 
+const isAccessTokenValid = async (token: string) => {
+  const statusCode = await userApi.isAccessTokenValid(token);
+  if (statusCode === 200) {
+    console.log('200 떴습니다! 유효한 토큰입니다.');
+    return true;
+  }
+  return false;
+};
+
 export const ContextsProvider = ({ children }: { children: ReactNode }) => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const isAccessTokenValid = async (token: string) => {
-      const statusCode = await userApi.isAccessTokenValid(token);
-      if (statusCode === 200) {
-        console.log('200 떴습니다! 유효한 토큰입니다.');
-        setIsLoggedIn(true);
-      }
-
-      setLoading(false);
-    };
-
     const accessToken = localStorage.getItem(ACCESS_TOKEN);
     if (accessToken !== null) {
-      isAccessTokenValid(accessToken);
+      const res = isAccessTokenValid(accessToken);
+      if (typeof(res) === 'boolean') {
+        setIsLoggedIn(res);
+      }
     }
   }, []);
-
-  if (loading === true) {
-    console.log('로그인이 유효한 유저인지 확인중입니다.');
-    return <div>Loading...</div>;
-  }
-
+  
   return (
     <LoginStateContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       {children}
